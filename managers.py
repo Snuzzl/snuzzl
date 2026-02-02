ef126a318901dd8987ffb51993037bb85054eca3
Rinalds Kalmanis <76496624+wizarin@users.noreply.github.com>
2026-02-02 13:43:49 +0000

    start of skeleton creation

---
+++ managers.py
@@ -0,0 +1,20 @@
+class TaskManager:
+    pass
+
+class MetricManager:
+    pass
+
+class CompetitionManager:
+    pass
+
+class RewardManager:
+    pass
+
+class AccountManager:
+    pass
+
+class SocialManager:
+    pass
+
+class DatabaseManager:
+    pass
