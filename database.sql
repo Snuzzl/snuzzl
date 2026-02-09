50e2dce8fcf26b0078e55a41f512bac76e24fb1b
dxransomz <dylanlangley94@gmail.com>
2026-02-09 13:18:46 +0000

    database 0.1

--- database.sql
+++ database.sql
@@ -4,16 +4,14 @@ CREATE TABLE users(
     username VARCHAR(50) UNIQUE NOT NULL,
     user_fname VARCHAR(30) NOT NULL,
     user_lname VARCHAR(30),
-    user_email VARCHAR(255) UNIQUE, 
-    user_phone VARCHAR(15) UNIQUE, 
+    user_email VARCHAR(255) UNIQUE,
+    user_phone VARCHAR(15) UNIQUE,
     user_dob DATE NOT NULL,
-    user_password_hash TEXT NOT NULL, 
-
+    user_password_hash TEXT NOT NULL,
     CONSTRAINT email_or_phone CHECK(
         COALESCE(NULLIF(user_email, ''), NULLIF(user_phone, '')) IS NOT NULL
     )
 );
-
 --- Tasks ---
 CREATE TABLE tasks(
     task_id SERIAL PRIMARY KEY,
@@ -26,46 +24,50 @@ CREATE TABLE tasks(
     routine_id INT,
     created_at TIMESTAMP DEFAULT NOW()
 );
-
 --- Metrics ---
 CREATE TABLE metrics (
     metric_id SERIAL PRIMARY KEY,
     user_id INT REFERENCES users(user_id),
     name VARCHAR(20) NOT NULL,
-    value NUMERIC(4,1) CHECK (value >= 0 AND value <= 10),
+    value NUMERIC(4, 1) CHECK (),
+    --Range--
     date DATE NOT NULL
 );
-
 --- Task Metric Intersection ---
 CREATE TABLE task_metrics (
     task_id INT REFERENCES tasks(task_id),
     metric_id INT REFERENCES metrics(metric_id),
     PRIMARY KEY (task_id, metric_id)
 );
-
 --- Specialized Tasks ---
 CREATE TABLE sleep (
     sleep_id SERIAL PRIMARY KEY,
     user_id INT REFERENCES users(user_id),
-    hours NUMERIC(4,1) CHECK (hours >= 0 AND hours <= 24),
-    quality INT CHECK (quality >= 0 AND quality <= 10),
+    hours NUMERIC(4, 1) CHECK (
+        hours >= 0
+        AND hours <= 24
+    ),
+    quality INT CHECK (
+        quality >= 0
+        AND quality <= 10
+    ),
     date DATE NOT NULL
 );
-
 CREATE TABLE steps (
     steps_id SERIAL PRIMARY KEY,
     user_id INT REFERENCES users(user_id),
-    steps INT CHECK (steps >= 0 AND steps <= 100000),
+    steps INT CHECK (
+        steps >= 0
+        AND steps <= 100000
+    ),
     date DATE NOT NULL
 );
-
 CREATE TABLE mood (
     mood_id SERIAL PRIMARY KEY,
     user_id INT REFERENCES users(user_id),
     feeling VARCHAR(50) NOT NULL,
     date DATE NOT NULL
 );
-
 --- Reminders ---
 CREATE TABLE reminders (
     reminder_id SERIAL PRIMARY KEY,
@@ -73,4 +75,15 @@ CREATE TABLE reminders (
     task_id INT REFERENCES tasks(task_id),
     text VARCHAR(100) NOT NULL,
     remind_at TIMESTAMP NOT NULL
+);
+--- Rewards ---
+CREATE TABLE rewards (
+    reward_id SERIAL PRIMARY KEY,
+    reward_name VARCHAR(50) NOT NULL,
+    reward_description VARCHAR(250),
+) --- User Rewards Intersection ---
+CREATE TABLE user_rewards (
+    user_id INT REFERENCES users(user_id),
+    reward_id INT REFERENCES rewards(reward_id),
+    PRIMARY KEY (user_id, reward_id)
 );
--- managers.py
+++ managers.py
@@ -15,13 +15,41 @@ class TaskManager:
         pass
 
 class MetricManager:
-    pass
+    def __init__(self):
+        pass
+
+    def add_metric(self, user_id, data):
+        pass
+
+    def delete_metric(self, user_id, data):
+        pass
+
 
 class CompetitionManager:
-    pass
+    def __init__(self):
+        pass
+
+    def create_competition(self, user_id, competition_id):
+        pass
+
+    def update_competition(self, user_id, competition_id):
+        pass
+
+    def delete_competition(self, user_id, competition_id):
+        pass
+
+    def add_to_competition(self, user_id, competition_id):
+        pass
 
 class RewardManager:
-    pass
+    def __init__(self):
+        pass
+
+    def give_reward(self, user_id, reward_id):
+        pass
+
+    def remove_reward(self, user_id, reward_id):
+        pass
 
 class AccountManager:
     pass
