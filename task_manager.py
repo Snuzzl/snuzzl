a116666166642b12271b61a23508f8ea380f4819
Harley Richardson <destructatron2018@gmail.com>
2026-02-16 13:10:09 +0000

    Add basic task manager class

--- task_manager.py
+++ task_manager.py
@@ -1,15 +1,41 @@
 class TaskManager:
-    def __init___(self):
-        pass
-
-    def create_task(self, user_id, data):
-        pass
+    def __init__(self):
+        self._tasks = []
     
-    def update_task(self, user_id, data):
-        pass
+    @property
+    def tasks(self):
+        return self._tasks
+    def add_task(self, task):
+        self._tasks.append(task)
 
-    def delete_task(self, user_id, data):
-        pass
+    def remove_task(self, index):
+        if index < len(self._tasks):
+            self._tasks.pop(index)
 
-    def get_tasks(self, user_id):
-        pass
+    def show_tasks(self):
+        for i in self._tasks:
+            print(f"Name: {i.name}, Description: {i.description}")
+
+class Task:
+    def __init__(self):
+        self._name = ""
+        self._description = ""
+        self._due = False
+    @property
+    def name(self):
+        return self._name
+    @property
+    def description(self):
+        return self._description
+    @property
+    def due(self):
+        return self._due
+    @name.setter
+    def name(self,newName):
+        self._name = newName
+    @description.setter
+    def description(self,newDescription):
+        self._description = newDescription
+    @due.setter
+    def due(self,newDue):
+        self._due = newDue
