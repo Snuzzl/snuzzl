35f01ce0a550e72a4ae2186f4232eeb88b111614
Harley Richardson <destructatron2018@gmail.com>
2026-02-17 15:26:50 +0000

    Add validation and some comments

--- task_manager.py
+++ task_manager.py
@@ -1,18 +1,23 @@
 class TaskManager:
     def __init__(self):
         self._tasks = []
-    
+    # We use property for getters, variable.setter for setters. This allows for validation when adding a task, or putting custom logic in the getter before returning something.
     @property
     def tasks(self):
         return self._tasks
     def add_task(self, task):
+        # If the task object we get isn't the same as Task, don't accept it.
+        if not isinstance(task, Task):
+            raise TypeError("Expected a task object")
         self._tasks.append(task)
 
     def remove_task(self, index):
+        # Don't take an index outside the actual length of the tasks array.
         if index < len(self._tasks):
             self._tasks.pop(index)
 
     def show_tasks(self):
+        # Nicer than just looking at self.tasks in a terminal, we can use this until we get the UI.
         for i in self._tasks:
             print(f"Name: {i.name}, Description: {i.description}")
 
