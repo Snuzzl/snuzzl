57db4f1ec7dc7d1c3d8bb0c2d2111d96de0a3866
ethanwilmott <up2276990@myport.ac.uk>
2026-02-23 13:31:46 +0000

    Update account_manager.py

--- account_manager.py
+++ account_manager.py
@@ -7,6 +7,7 @@ class AccountManager:
     def login(self):
         # If username and hashed password match the values in db then self.currentUser = username
         # Password can be worked out later
+        #new log in systme will have to be made to work with hashing and db
         print("---Login Page---")
         username = input("Enter username: ")
         password = input("Enter password: ")
@@ -22,14 +23,17 @@ class AccountManager:
         pass
 
     def logout(self):
+        self.currentUser = "Guest"
         return self.login()
+        #sets to guest so when logged out information can not be accessed
 
     def createAccount(self, account):
         self.accounts.append(account)
         self.username = account.username
         self.currentUser = account.username
         print(f"Account '{account.username}' created and added to list")
- 
+        #the user information is stored like htis for now for testing 
+
     def deleteAccount(self, username):
         for i, account in enumerate(self.accounts):
             if account.username == username:
@@ -40,12 +44,25 @@ class AccountManager:
                 return True
         print(f"Account '{username}' not found")
         return False
-
-    def updateEmail():
-        pass
-
-    def updatePasword():
-        pass
+   
+    def updateEmail(self, account):
+        new_email = input("Enter new email: ")
+        account.email = new_email
+        print(f"Email updated to {new_email}")
+        return True
+              
+    def updatePassword(self):
+        password = input("Enter current password: ")
+        if account.password == password:
+            new_password = input("Enter new password: ")
+            for account in self.accounts:
+                if account.username == self.currentUser:
+                    account.password = new_password
+                    print(f"Password updated to {new_password}")
+                    return True
+        else:
+            print("Incorrect current password")
+            return False
 
     def sync():
         # Sync current user account to db, can be left for later
@@ -60,8 +77,10 @@ class AccountManager:
             print(f"- {account.username} ({account.email})")
         print()
 
-
 class Account:
+    #validates all inputs
+    # thought it was cleaner within a seperate class but can be moved to account manager if needed
+
     def __init__(self, username, email, password):
         self.username = self._validateUsername(username)
         self.email = self._validateEmail(email)
@@ -91,32 +110,29 @@ class Account:
     def print_details(self):
         print(self)
 
-
-
-
 #testing
 def run():
     account_manager = AccountManager()
-    
+
     # Create accounts
     account1 = Account("test1", "test1@example.com", "test123")
     account_manager.createAccount(account1)
     account1.print_details()
-    
+  
     account2 = Account("admin", "admin@example.com", "admin123")
     account_manager.createAccount(account2)
     account2.print_details()
-    
+ 
     account3 = Account("user2", "user2@example.com", "pass456")
     account_manager.createAccount(account3)
-    
+   
     # Print all accounts
     account_manager.print_all_accounts()
-    
+  
     # Delete an account
     print("Deleting test1")
     account_manager.deleteAccount("test1")
-    
+  
     #pritning accounts
     account_manager.print_all_accounts()
     print("All usernames:", account_manager.get_all_accounts())
@@ -125,18 +141,13 @@ def run():
     account_manager.deleteAccount("test1")
     account_manager.print_all_accounts()
 
-
     #login test
     print("Logging in as admin")
     account_manager.login()
 
-    #test log out
-    print("Logging out")
-    account_manager.logout()
-
-    #test logging back in
-    print("Logging out")
-    account_manager.logout()
+    account_manager.print_all_accounts()
+    account_manager.updateEmail()
+    account_manager.updatePassword()
 
 if __name__ == "__main__":    
     run()
